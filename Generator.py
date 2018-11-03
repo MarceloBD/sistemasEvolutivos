cfcfa77bb98e16ff3dcda3a9626bd8bbea0a7d89
MarceloBD <marcelodianib@gmail.com>
2018-11-02 23:45:01 -0300

    Making new generations of generators

--- Generator.py
+++ Generator.py
@@ -7,16 +7,18 @@ class Generator():
 		self.dis = dis 
 		self.product = np.zeros(10)
 		self.mutation_chance = 0.5
+		self.hit = 0 
 		return
 
 	def sell(self):
 		self.mutation()
 		result = self.dis.buy(self.product)
 		if(result):
-			print('sold')
+			#print('sold')
+			self.hit += 1
 			return 1
 		else:
-			print('not sold')
+			#print('not sold')
 			return 0
 
 	def mutation(self):
@@ -24,3 +26,14 @@ class Generator():
 		if (x <= 100*self.mutation_chance):
 			index = random.randint(0,9)
 			self.product[index] = ~int(self.product[index]) + 2
+
+	def get_hit(self):
+		return self.hit
+
+	def print_product(self):
+		print (self.product)
+
+	def crossover(self, mask):
+		for i in range(len(mask)):
+			if(mask[i] == 0):
+				self.product[i] = ~int(self.product[i]) + 2
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from Generator import Generator
 from Discriminator import Discriminator
+import numpy as np 
+import copy
 
+def selection(gen):
+	values = [gen[i].hit for i in range(50)]
+	max_index = np.argsort(-np.array(values))[:5]
+	max_gen = [gen[i] for i in max_index]
+	new_gen = copy.deepcopy(gen)
+	for i in range(50):
+		new_gen[i] = copy.deepcopy(max_gen[int(i/10)])
+	return new_gen
+
+def crossover(gen):
+	for i in range(50):
+		gen[i].crossover([0,1,1,0,1,1,1,1,1,0])
+	return gen 		
+
+def run(gen):
+	for epoch in range(50):
+		for i in range(50):
+			gen[i].sell()
 
 if __name__ == '__main__':
 	gen = []
@@ -8,9 +28,35 @@ if __name__ == '__main__':
 	
 	for i in range(50):
 		gen.append(Generator(dis))
-	not_sold = 1
-	while(not_sold):
-		for i in range(50):
-			if (gen[i].sell()):
-				not_sold = 0
+	
+	run(gen)
 
+	[print(gen[i].get_hit()) for i in range(50)]
+
+	new_gen = selection(gen)
+	gen = crossover(new_gen)
+
+	run(gen)
+
+	[print(gen[i].get_hit()) for i in range(50)]
+
+	new_gen = selection(gen)
+	gen = crossover(new_gen)
+
+	run(gen)
+
+	[print(gen[i].get_hit()) for i in range(50)]
+
+	new_gen = selection(gen)
+	gen = crossover(new_gen)
+
+	run(gen)
+
+	[print(gen[i].get_hit()) for i in range(50)]
+
+	new_gen = selection(gen)
+	gen = crossover(new_gen)
+
+	run(gen)
+
+	[print(gen[i].get_hit(), gen[i].print_product()) for i in range(50)]
